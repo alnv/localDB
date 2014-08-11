baf5330b955993c107d1161d26764be35388dce0
Alexander Naumov <naumov@tozen.de>
2014-08-11 11:59:46 +0200

    delete not used tasks

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,27 @@
 module.exports = function(grunt){
 	
 	grunt.initConfig({
+		
+		/*
+		concat: {
+			
+			options: {
+				
+				seperator: ";"
+				
+			},
+			
+			target: {
+				
+				src: [],
+				dest: "dest/main.js"
+				
+			}
+			
+			
+		},
+		*/
+		
 		uglify: {
 			
 			options: {
@@ -8,27 +29,34 @@ module.exports = function(grunt){
 				compress: true,
 				sourceMap: 'dest/beagle.map',
 			},
+			
 			target: {
 				src: 'src/main.js',
 				dest: 'dest/beagle.min.js'
 			}
 			
 		},
+		
 		watch: {
+		
 			script: {
+			
 				files: ['src/*.js'],
 				tasks: ['uglify'],
 				options: {
 					livereload: 3111
 				}
+				
 			}
+			
 		}
+		
 	});
 	
 	
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	
-	grunt.registerTask("default", ['concat', 'uglify']);
+	grunt.registerTask("default", ['uglify']);
 	
 }
